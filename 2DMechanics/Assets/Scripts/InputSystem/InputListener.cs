b6fa945c2bc0a75e01c493b60a66f5902123ab74
ITHUB\Student <qew>
2025-02-18 12:37:35 +0300

    addCore

---
+++ 2DMechanics/Assets/Scripts/InputSystem/InputListener.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputListener : MonoBehaviour
+{
+    private MainControl inputActions;
+    private Rigidbody2D rb;
+    [SerializeField] private PlayerMovement player;
+    void Awake()
+    {
+        inputActions = new();
+        inputActions.Player.Move.performed += Move;
+    }
+
+    private void Move(InputAction.CallbackContext context)
+    {
+        var inputValue = context.ReadValue<Vector2>().normalized;
+        player.Move(inputValue);
+    }
+}
---
+++ 2DMechanics/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D _rb;
+
+    public void Move(Vector2 direction)
+    {
+        
+    }
+}
